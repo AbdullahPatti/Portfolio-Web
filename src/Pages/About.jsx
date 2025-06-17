7f7032df67d7cf0821dbb30b61fb831b532ec763
Abdullah Haroon <khukhi321@gmail.com>
2025-06-18 00:15:23 +0500

    new code

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,8 @@
+import "./About.css"
 function About() {
   return (
     <section>
-      <h2>About Me</h2>
+      <h2 className="skillset">My Skills</h2>
       <p>I am a student at FAST NUCES Lahore. I enjoy building modern web apps and editing high-quality videos.</p>
     </section>
   );
