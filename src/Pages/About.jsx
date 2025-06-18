6f3f80beb0bfb2464a67c144572aa678ef1ab93a
Abdullah Haroon <khukhi321@gmail.com>
2025-06-18 17:09:25 +0500

    updates

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,10 @@
 import "./About.css"
 function About() {
   return (
-    <section>
+    <section className="abouts">
       <h2 className="skillset">My Skills</h2>
-      <p>I am a student at FAST NUCES Lahore. I enjoy building modern web apps and editing high-quality videos.</p>
+      <p>I have a deep interest in exploring the intersection of technology, creativity, and personal growth. I'm drawn to building things that solve problems, tell stories, or inspire others. Whether it's through logical thinking, visual expression, or meaningful conversations, I enjoy engaging with ideas that challenge me and help me evolve. I'm passionate about learning, experimenting, and continuously pushing my boundaries to better understand both myself and the world around me.
+</p>
     </section>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,26 +4,30 @@ function Home() {
   return (
     <section className="intro-section">
       <h1 className="headline">I code, I create, I narrate</h1>
-      <h3 className='subheading'>I’m Abdullah Haroon</h3> 
+      <h3 className="subheading">I’m Abdullah Haroon</h3>
 
       <div className="intro-content">
         <div className="text-content">
           <p className="bio">
-           I am a 20-year-old from Lahore with a mind constantly brimming with thoughts and curiosity. I see myself as someone in the process of becoming the main character in my own journey, striving to grow, learn, and lead with purpose. Currently, I am pursuing a Bachelor's degree in Computer Science from FAST NUCES, Lahore. My passion lies in the ever-evolving world of technology and the changing ways we interact with it.
-<br />
-<br />
-I enjoy writing code as much as I enjoy meaningful conversations. I have developed skills in C, C++, MySQL, HTML, CSS, and React.JS, and I love using these tools to bring ideas to life. I have also actively participated in coding competitions such as the ACM Speed Programming Competition and the SOFTEC SQL Query Master, which have sharpened my problem-solving abilities and exposed me to fast-paced, real-world challenges.
-<br />
-<br />
-Outside the academic and competitive space, I am drawn to storytelling and photography, which allow me to express creativity, capture unique perspectives, and connect with others on a deeper level. I believe in continuous self-discovery and in pushing my boundaries to better understand both myself and the world around me.
-<br />
-<br />
-With every project, competition, and line of code, I aim to grow not just as a Computer Science student, but as a thoughtful and impact-driven individual ready to contribute meaningfully to the tech world.
-
-
+            I am a 20-year-old from Lahore with a mind constantly brimming with thoughts and curiosity. I see myself as someone in the process of becoming the main character in my own journey, striving to grow, learn, and lead with purpose. Currently, I am pursuing a Bachelor's degree in Computer Science from FAST NUCES, Lahore. My passion lies in the ever-evolving world of technology and the changing ways we interact with it.
+            <br />
+            <br />
+            I enjoy writing code as much as I enjoy meaningful conversations. I have developed skills in C, C++, MySQL, HTML, CSS, and React.JS, and I love using these tools to bring ideas to life. I have also actively participated in coding competitions such as the ACM Speed Programming Competition and the SOFTEC SQL Query Master, which have sharpened my problem-solving abilities and exposed me to fast-paced, real-world challenges.
+            <br />
+            <br />
+            Outside the academic and competitive space, I am drawn to storytelling and photography, which allow me to express creativity, capture unique perspectives, and connect with others on a deeper level. I believe in continuous self-discovery and in pushing my boundaries to better understand both myself and the world around me.
+            <br />
+            <br />
+            With every project, competition, and line of code, I aim to grow not just as a Computer Science student, but as a thoughtful and impact-driven individual ready to contribute meaningfully to the tech world.
           </p>
         </div>
-        <img src="pattipromax.jpg" alt="No Image here" className="ppm" />
+
+        <div className="ppm-container">
+          <img src="pattipromax.jpg" alt="No Image here" className="ppm" />
+          <a href="https://linktr.ee/abdullahpatti?utm_source=linktree_profile_share&ltsid=f18c952d-332f-4d07-a1d2-110b51c2f023" target="_blank" rel="noopener noreferrer" className="ppm-button">
+            LinkTree
+          </a>
+        </div>
       </div>
     </section>
   );
